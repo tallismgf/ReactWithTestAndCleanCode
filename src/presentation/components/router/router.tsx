0b6cddc14ac58520c17fe9a7aa89afa03fceed75
tallismgf <tallismgf@hotmail.com>
2021-12-30 18:33:26 -0300

    feat: ensure Login disabled submit button only if form is invalid

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Login } from '@/presentation/pages'
+import { ValidationStub } from '@/presentation/test'
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login validation={new ValidationStub()} />} />
       </Routes>
     </BrowserRouter>
   )
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -41,7 +41,7 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
 
           <button
             data-testid="submit"
-            disabled
+            disabled={ !!state.emailError || !!state.passwordError }
             className={Styles.submit}
             type="submit"
           >
