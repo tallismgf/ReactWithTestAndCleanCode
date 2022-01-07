a45ac5abef270bf7f18714922891af708c73f1f8
tallismgf <tallismgf@hotmail.com>
2022-01-07 14:46:09 -0300

    test: ensure Input Component removes reandOnly on focus

--- src/presentation/components/input/input.spec.tsx
+++ src/presentation/components/input/input.spec.tsx
@@ -1,20 +1,30 @@
 import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
+import { fireEvent, render, RenderResult } from '@testing-library/react'
+import faker from 'faker'
 import Input from './input'
 import Context from '@/presentation/contexts/form/form-context'
 
-const makeSut = (): RenderResult => {
+const makeSut = (fieldName: string): RenderResult => {
   return render(
     <Context.Provider value={{ state: {} }}>
-      <Input name="field" />
+      <Input name={fieldName} />
     </Context.Provider>
   )
 }
 
 describe('Input Component', () => {
   test('Should begin with reandOnly', () => {
-    const { getByTestId } = makeSut()
-    const input = getByTestId('field') as HTMLInputElement
+    const field = faker.database.column()
+    const { getByTestId } = makeSut(field)
+    const input = getByTestId(field) as HTMLInputElement
     expect(input.readOnly).toBe(true)
+  })
+
+  test('Should remove reandOnly on focus', () => {
+    const field = faker.database.column()
+    const { getByTestId } = makeSut(field)
+    const input = getByTestId(field) as HTMLInputElement
+    fireEvent.focus(input)
+    expect(input.readOnly).toBe(false)
   })
 })
